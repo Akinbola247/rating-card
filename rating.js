ac5dcdbe6c9faa40c69462e16d7f1b2a83913ba0
Akinbola Kehinde <112096641+Akinbola247@users.noreply.github.com>
2022-09-18 17:20:10 -0400

    Update rating.js

--- rating.js
+++ rating.js
@@ -1,55 +1,27 @@
 
-let rate = document.querySelectorAll(".rate");
-let rate1 = document.querySelector('.rate1');
-let rate2 = document.querySelector('.rate2');
-let rate3 = document.querySelector('.rate3');
-let rate4 = document.querySelector('.rate4');
-let rate5 = document.querySelector('.rate5');
-let submit = document.querySelector('.submit');
-let box = document.querySelector('.box');
 let ratedval = document.querySelector('.ratedval');
 
+const timeRangeWrapper = document.querySelector('#text');
+const timeOptions = timeRangeWrapper.querySelectorAll('.rate');
+    timeOptions.forEach((item, i) => {
+        const domIndex = item.getAttribute('data-item');
+        item.addEventListener('click', () => {
+            ratedval.innerText = item.innerText;
+            timeOptions.forEach(option => {
+              option.classList.remove('pressed');
+            })
+            if (i == domIndex) {
+                item.classList.add('pressed');
+            }
+        });
+    });
 
-let buttons = Array.from(document.querySelectorAll('.rate'));
-buttons.push(submit);
-
-buttons.map(buttons => {
-    buttons.addEventListener('click', (e) => { 
-        switch (e.target.innerText){
-        case '1':            
-                ratedval.innerText = e.target.innerText;             
-            break;
-        case '2':
-                ratedval.innerText = e.target.innerText;
-            break;
-        case '3':
-                ratedval.innerText = e.target.innerText;
-        break;
-        case '4':
-                ratedval.innerText = e.target.innerText;
-        break;
-        case '5':   
-                ratedval.innerText = e.target.innerText;
-        break;
-
-        case 'SUBMIT': 
-        if(!ratedval.innerText){
-            alert("please make a selection");       
-        }else{
-            box = box.classList.add('invisible');
+function myFunction() {
+        var box = document.querySelector(".box");
+        if (box.style.display === "none") {
+          box.style.display = "block";
+        } else {
+          box.style.display = "none";
         }
-        break;
-        default:
-       }
+      }
 
-    });  
-});
-
-
-
-function myFunction(rat) {
-        rat.classList.toggle("pressed"); 
- }
-
-
- 
